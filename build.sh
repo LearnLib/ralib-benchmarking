40a02feda98a09789a699eadebdb70efcde26f77
Simon Dierl <simon.dierl@cs.tu-dortmund.de>
2023-12-22 10:00:48 +0100

    discussion results

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-cd ralib && mvn -Dmaven.repo.local=../m2 package assembly:single
-
+cd ralib
+if [ -e ../m2 ]; then
+  mvn -Dmaven.repo.local=../m2 package assembly:single
+else
+  mvn package assembly:single
+fi
--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-cd packages && sudo dpkg -i *.deb
+if [ -e packages ]; then
+  cd packages && sudo dpkg -i *.deb
+else
+  echo "No packages directory found."
+  echo "This script is only needed for artifact evaluation."
+  echo "When using this repository normally, it should not be run."
+fi
 
